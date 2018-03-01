f88c86ab71e4111fdd98c9f4b492e4a864949acd
Daniel <dmella04@gmail.com>
2018-03-01 15:23:46 -0300

    Prueba para Servidor

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,46 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
 
-server 'localhost', port: 3000, roles: [:web, :app, :db], primary: true
+server 'ec2-18-218-149-45.us-east-2.compute.amazonaws.com', port: 22, roles: [:web, :app, :db], primary: true
 
-set :deploy_to, "/var/www/test"
+#server 'localhost', roles: [:web, :app, :db], primary: false
 
-set :repo_url,        'git@github.com:ignotummisterrii/test.git'
-set :application,     'test'
-set :user,            'misterri'
+#set :deploy_to, "/mnt/c/Users/#{fetch(:user)}/beetrack/"
+
+set :repo_url, 'git@github.com:ignotummisterrii/test.git'
+
+set :application, 'test'
+set :user, 'ubuntu'
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}/"
+
+
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-
-set :pty,             true
+set :pty,             false
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:misterri), keys: %w(~/.ssh/id_rsa.pub) }
+
+set :puma_bind,       "unix://#{fetch(:deploy_to)}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "/var/tmp/pids/puma.state"
+set :puma_pid,        "/var/tmp/pids/puma.pid"
+set :puma_access_log, "/var/log/puma.error.log"
+set :puma_error_log,  "/var/log/puma.access.log"
+
+
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 
 namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
+  desc 'Create Directories for Puma Pids and Socket' 
+
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir /var/tmp/sockets -p"
+      execute "mkdir /var/tmp/pids -p"
     end
   end
 
@@ -42,6 +49,7 @@ end
 
 
 namespace :deploy do
+  
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
@@ -69,7 +77,8 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
+
+  #before :starting,     :check_revision
   after  :finishing,    :compile_assets
   after  :finishing,    :cleanup
   after  :finishing,    :restart
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+#set :stage, :development
+#set :branch, "master"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -17,7 +20,7 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
+ #role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
@@ -39,13 +42,13 @@
 # limited set of options, consult the Net::SSH documentation.
 # http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
 #
-# Global options
-# --------------
+
+
 #  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    keys: %w(~/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
-#  }
+# }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
@@ -59,3 +62,10 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: ["publickey"],
+  keys: ["/mtn/c/Users/zesho/Desktop/daniel.pem"]
+}
